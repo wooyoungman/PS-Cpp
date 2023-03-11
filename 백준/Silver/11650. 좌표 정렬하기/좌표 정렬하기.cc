f857210073adb0fe03e15f30caabb6cbe28d5a00
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-03-11 14:23:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,43 +1,27 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
+#include <vector>
 using namespace std;
-// 구조체 풀이
-
-typedef struct {
-	int x;
-	int y;
-}pos;
-
-bool cmp(pos& p1, pos& p2)
-{
-	if (p1.x == p2.x)
-	{
-		return p1.y < p2.y;
-	}
-	else
-	{
-		return p1.x < p2.x;
-	}
-}
+// 벡터 풀이 (벡터에서 sort를 이용하면 first 기준으로 정렬
+// first가 같으면 알아서 second를 비교
+vector <pair<int, int>> v;
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n;
+	int n,x,y;
 	cin >> n;
-	pos* arr = new pos[n];
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i].x >> arr[i].y;
+		cin >> x >> y;
+		v.push_back({x, y});
 	}
-	sort(arr, arr + n,cmp);
+	sort(v.begin(), v.end());
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i].x << ' ' << arr[i].y << "\n";
+		cout << v[i].first << ' ' << v[i].second << "\n";
 	}
-	delete[] arr;
 	return 0;
 }
