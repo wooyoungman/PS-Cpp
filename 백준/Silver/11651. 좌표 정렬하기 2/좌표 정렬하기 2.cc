4854f9724faebde7480a99a3842e9a8b700a45a3
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-03-12 16:07:35 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -3,40 +3,62 @@
 #include <vector>
 
 using namespace std;
+// 구조체 풀이
+//typedef struct {
+//	int x;
+//	int y;
+//}pos;
+//
+//bool cmp(pos& p1, pos& p2)
+//{
+//	if (p1.y == p2.y)
+//	{
+//		return p1.x < p2.x;
+//	}
+//	else
+//	{
+//		return p1.y < p2.y;
+//	}
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n;
+//	cin >> n;
+//	pos* arr = new pos[n];
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i].x >> arr[i].y;
+//	}
+//	sort(arr, arr + n, cmp);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << arr[i].x << ' ' << arr[i].y << "\n";
+//	}
+//	return 0;
+//}
 
-typedef struct {
-	int x;
-	int y;
-}pos;
-
-bool cmp(pos& p1, pos& p2)
-{
-	if (p1.y == p2.y)
-	{
-		return p1.x < p2.x;
-	}
-	else
-	{
-		return p1.y < p2.y;
-	}
-}
+vector <pair<int, int>> v;
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n;
+	int n,x,y;
 	cin >> n;
-	pos* arr = new pos[n];
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i].x >> arr[i].y;
+		cin >> x >> y;
+		v.push_back({y, x});
 	}
-	sort(arr, arr + n, cmp);
+	sort(v.begin(), v.end());
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i].x << ' ' << arr[i].y << "\n";
+		cout << v[i].second << ' ' << v[i].first << "\n";
 	}
 	return 0;
 }
