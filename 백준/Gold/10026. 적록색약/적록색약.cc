7d812972a476a64ebf72f3efd2c7290192ea7300
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-03-28 19:14:59 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,115 +1,41 @@
-#include <iostream>
-#include <queue>
-#include <cstring>
+#include<iostream>
+#include<cstring>
+#include<queue>
+
 using namespace std;
-int dx[4] = { 0,0,1,-1 };
+
+
+// bfs 함수는 하나로 구현한다. (인접해 있는 경우에만 큐에 넣는 방법)
+// 그리고 main 함수에서 for문을 이용하여 탐색할 때
+// 적록색약인 경우와 아닌경우를 나누어 R=G를 처리하여 탐색한다.
+int dx[4] = { 0,0,-1,1 };
 int dy[4] = { 1,-1,0,0 };
-char map[101][101];
 bool visited[101][101];
-int n,cnt_R=0,cnt_G=0,cnt_B=0,cnt_RG=0,cnt_RGB=0;
-
-void bfs_R(int x, int y)
-{
-	queue<pair<int,int>>q;
-	q.push(make_pair(x, y));
-	while (!q.empty())
-	{
-		int cur_x = q.front().first;
-		int cur_y = q.front().second;
-		visited[cur_x][cur_y] = true;
-		q.pop();
-		for (int i = 0; i < 4; i++)
-		{
-			int next_x = cur_x + dx[i];
-			int next_y = cur_y + dy[i];
-			if (next_x >= 0 && next_y >= 0 && next_x < n && next_y < n && visited[next_x][next_y] == false)
-			{
-				if (map[next_x][next_y] == 'R')
-				{
-					q.push(make_pair(next_x, next_y));
-					visited[next_x][next_y] = true;
-				}
-			}
-		}
-	}
-}
-void bfs_G(int x, int y)
+char map[101][101];
+int cnt = 0,n;
+void bfs(int x, int y)
 {
 	queue<pair<int, int>>q;
 	q.push(make_pair(x, y));
+	visited[x][y] = true;
 	while (!q.empty())
 	{
 		int cur_x = q.front().first;
 		int cur_y = q.front().second;
-		visited[cur_x][cur_y] = true;
 		q.pop();
 		for (int i = 0; i < 4; i++)
 		{
 			int next_x = cur_x + dx[i];
 			int next_y = cur_y + dy[i];
-			if (next_x >= 0 && next_y >= 0 && next_x < n && next_y < n && visited[next_x][next_y] == false)
+			if (next_x >= 0 && next_y >= 0 && next_x < n && next_y < n && visited[next_x][next_y] == false && map[cur_x][cur_y] == map[next_x][next_y])
 			{
-				if (map[next_x][next_y] == 'G')
-				{
-					q.push(make_pair(next_x, next_y));
-					visited[next_x][next_y] = true;
-				}
+				q.push(make_pair(next_x, next_y));
+				visited[next_x][next_y] = true;
 			}
 		}
 	}
 }
 
-void bfs_B(int x, int y)
-{
-	queue<pair<int, int>>q;
-	q.push(make_pair(x, y));
-	while (!q.empty())
-	{
-		int cur_x = q.front().first;
-		int cur_y = q.front().second;
-		visited[cur_x][cur_y] = true;
-		q.pop();
-		for (int i = 0; i < 4; i++)
-		{
-			int next_x = cur_x + dx[i];
-			int next_y = cur_y + dy[i];
-			if (next_x >= 0 && next_y >= 0 && next_x < n && next_y < n && visited[next_x][next_y] == false)
-			{
-				if (map[next_x][next_y] == 'B')
-				{
-					q.push(make_pair(next_x, next_y));
-					visited[next_x][next_y] = true;
-				}
-			}
-		}
-	}
-}
-
-void bfs_RG(int x, int y)
-{
-	queue<pair<int, int>>q;
-	q.push(make_pair(x, y));
-	while (!q.empty())
-	{
-		int cur_x = q.front().first;
-		int cur_y = q.front().second;
-		visited[cur_x][cur_y] = true;
-		q.pop();
-		for (int i = 0; i < 4; i++)
-		{
-			int next_x = cur_x + dx[i];
-			int next_y = cur_y + dy[i];
-			if (next_x >= 0 && next_y >= 0 && next_x < n && next_y < n && visited[next_x][next_y] == false)
-			{
-				if (map[next_x][next_y] == 'G'||map[next_x][next_y]=='R')
-				{
-					q.push(make_pair(next_x, next_y));
-					visited[next_x][next_y] = true;
-				}
-			}
-		}
-	}
-}
 
 int main()
 {
@@ -125,40 +51,37 @@ int main()
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (visited[i][j] == false&&map[i][j]=='R')
+			if (visited[i][j] == false)
 			{
-				bfs_R(i, j);
-				cnt_R++;
-			}
-			else if (visited[i][j] == false && map[i][j] == 'G')
-			{
-				bfs_G(i, j);
-				cnt_G++;
-			}
-			else if (visited[i][j] == false && map[i][j] == 'B')
-			{
-				bfs_B(i, j);
-				cnt_B++;
+				bfs(i, j);
+				cnt++;
 			}
 		}
 	}
+	cout << cnt << "\n";
+	cnt = 0;
 	memset(visited, 0, sizeof(visited));
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (visited[i][j] == false && (map[i][j] == 'R'||map[i][j]=='G'))
+			if (map[i][j]=='R')
 			{
-				bfs_RG(i, j);
-				cnt_RG++;
-			}
-			else if (visited[i][j] == false && map[i][j] == 'B')
-			{
-				bfs_B(i, j);
-				cnt_RGB++;
+				map[i][j] = 'G';
 			}
 		}
 	}
-	cout << cnt_R+cnt_G+cnt_B<<"\n"<<cnt_RG+cnt_RGB;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (visited[i][j] == false)
+			{
+				bfs(i, j);
+				cnt++;
+			}
+		}
+	}
+	cout <<cnt;
 	return 0;
 }
