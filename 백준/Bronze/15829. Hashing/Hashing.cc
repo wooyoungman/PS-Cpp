3a38dbd617d26ea42c11c15dd2ce1c733a839090
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-04-01 18:11:26 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -9,15 +9,14 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int l;
-	unsigned int sum = 0;
+	long long sum = 0;
 	string s;
 	cin >> l >> s;
 	for (int i = 0; i < l; i++)
 	{
 		// a의 아스키 값 97 이용
-		unsigned int q = (unsigned int)pow(31, i);
+		long long q = (long long)pow(31, i)%1234567891;
 		sum+= (int(s[i]) - 96) * q;
-		sum %= 1234567891;
 	}
 	cout << sum;
 	return 0;
