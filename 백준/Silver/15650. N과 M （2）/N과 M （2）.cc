5b69493da58f5dd1fb0b06cfadcfbb8ce24f147a
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-07-26 17:38:38 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[10];
+int ans[10];
+bool check[10];
+
+// 출력 함수
+
+void print_ans() {
+	for (auto i : ans) {
+		if (i != 0) {
+			cout << i << " ";
+		}
+	}
+	cout << "\n";
+}
+
+// 재귀 함수
+void recur(int depth,int start) {
+
+	// 종료 조건
+	if (depth == M) {
+		print_ans();
+		return;
+	}
+
+	// 수행 부분
+	for (int i = start; i <= N; i++) {
+		if (!check[i]) {
+			ans[depth] = arr[i];
+			check[i] = true;
+			recur(depth + 1,i+1);
+			check[i] = false;
+		}
+	}
+
+}
+
+
+
+
+
+int main() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		arr[i] = i;
+	}
+	recur(0,1);
+	return 0;
+}
