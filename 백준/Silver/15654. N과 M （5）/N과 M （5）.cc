fd7c829c2a81279837c829f6edcd217cb89dc6ff
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-07-28 10:30:14 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int N, M;
+vector<int> v;
+int ans[10];
+bool check[10];
+
+void print_ans() {
+	for (auto i : ans) {
+		if (i != 0) {
+			cout << i << " ";
+		}
+	}
+	cout << "\n";
+}
+
+void recur(int depth) {
+	if (depth == M) {
+		print_ans();
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!check[i]) {
+			ans[depth] = v[i];
+			check[i] = true;
+			recur(depth + 1);
+			check[i] = false;
+		}
+	}
+}
+
+
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	recur(0);
+	return 0;
+}
