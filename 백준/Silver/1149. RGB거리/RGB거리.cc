cf98fa3ff8e3f5c56273ff57d132df720b2a3c40
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-08-09 08:41:57 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,23 +1,20 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+
 using namespace std;
 
-int n;
-int arr[1001][3];
-int ans = 0;
+int n,ans;
+int dp[1001][3];
 
 int main() {
-
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-
-		arr[i][0] = arr[i][0] + min(arr[i-1][1], arr[i-1][2]);
-		arr[i][1] = arr[i][1] + min(arr[i-1][0], arr[i-1][2]);
-		arr[i][2] = arr[i][2] + min(arr[i-1][0], arr[i-1][1]);
+		cin >> dp[i][0] >> dp[i][1] >> dp[i][2];
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2];
 	}
-	ans = min(arr[n][0], arr[n][1]);
-	ans = min(ans, arr[n][2]);
+	ans = min(dp[n][0], dp[n][1]);
+	ans = min(ans, dp[n][2]);
 	cout << ans;
 	return 0;
 }
