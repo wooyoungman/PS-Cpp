dc8af354457ccf8b1054b326b842fd0218eeef92
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-11-11 11:59:21 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,781 ms, Memory: 21,744 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/README.md
@@ -0,0 +1,15 @@
+# [D2] 백만 장자 프로젝트 - 1859 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LrsUaDxcDFAXc) 
+
+### 성능 요약
+
+메모리: 21,744 KB, 시간: 1,781 ms, 코드길이: 2,235 Bytes
+
+### 제출 일자
+
+2023-11-11 11:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.cpp
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T,n,num;
+	cin>>T;
+
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		cin>>n;
+        vector<int>v;
+        long long answer=0;
+        for(int i=0;i<n;i++){
+            cin>>num;
+            v.push_back(num);
+        }
+        
+		int temp_max=v[n-1];
+        for(int i=n-1;i>=0;i--){
+            if(v[i]<=temp_max){
+            	answer+=(temp_max-v[i]);    
+            }
+            else{
+                temp_max=v[i];
+            }
+        }
+        cout<<"#"<<test_case<<" "<<answer<<"\n";
+	}
+	return 0;
+}
