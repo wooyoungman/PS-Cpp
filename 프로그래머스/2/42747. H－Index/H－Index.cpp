7d569563b72939b354e7c318a4fd402e1c9ff2c3
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-01-08 21:59:28 +0900

    Update H－Index.cpp

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -5,16 +5,21 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    sort(citations.begin(), citations.end(), greater<int>()); // 내림차순으로 정렬
+    sort(citations.begin(), citations.end(), greater<int>()); 
 
-    int hIndex = 0;
-    for (int i = 0; i < citations.size(); ++i) {
-        if (citations[i] >= i + 1) {
-            hIndex = i + 1;
-        } else {
+    int answer = 0;
+    
+    // 내림차순 정렬 후 인용 횟수와 현재 인덱스 값을 비교하며 
+    // 최대인 경우를 구하기 (인용 횟수 >= 현재 인덱스 + 1)인 경우 반복문 진행
+    // 현재 인덱스 + 1이 h번 이상 인용된 논문의 개수를 뜻함
+    for(int i=0;i<citations.size();i++){
+        if(citations[i]>=i+1){
+            answer=i+1;
+        }
+        else{
             break;
         }
     }
-
-    return hIndex;
+    
+    return answer;
 }
