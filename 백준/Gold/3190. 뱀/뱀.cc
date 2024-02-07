842fe5956777c47009bcc0bd36c1764c31b069c2
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-02-07 23:40:11 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,149 +1,136 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <string>
 #include <queue>
-#include <stack>
 #include <algorithm>
-#define INF 987654321
 using namespace std;
 
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
-int map[100][100];
+int n, k, l, t, y, x;
+char c;
+int map[101][101];
+bool visited[101][101];
+queue<pair<int, char>>dir_changes;
+queue<pair<int, char>>tails;
+// 뱀은 머리를 하나 늘려 다음칸으로 이동한다.
+// 다음칸의 사과를 먹는다? -> 아무 이상 없음 (몸길이 +1)
+// 다음칸의 사과를 안먹는다? -> 꼬리를 자른다. (꼬리 칸을 비운다, 몸길이 그대로)
+// 현재 뱀의 몸이 있는 부분을 visited를 true로
+// 만약 사과를 안먹으면 꼬리를 false로
 
-deque<pair<int, int>> snake;
-
-int N, K, L;
-int main()
-{
-	cin >> N;
-	cin >> K;
-	int a, b;
-	char c;
-	for (int i = 0; i < K; i++)
-	{
-		scanf("%d %d", &a, &b);
-		map[a-1][b-1] = 1;
-	}
-
-
-	cin >> L;
-	snake.push_front({ 0,0 });
-	int state = 1;
-	int seconds = 0;
-	map[0][0] = 2;
-	int dir = 1;
-	int x = 0;
-	int y = 0;
-
-	for (int i = 0; i < L; i++)
-	{
-		scanf("%d %c", &a, &c);
-		while(seconds<a)
-		{
-			
-			int ny = snake.front().first + dy[dir];
-			int nx = snake.front().second +dx[dir];
-			//cout << ny << " " << nx << " " << dir<<endl;
-			if (ny<0 || nx<0 || ny>N - 1 || nx>N - 1||map[ny][nx]==2)
-			{
-				state = 0;
-				break;
-			}
-			
-			if (map[ny][nx] == 1)
-			{
-				snake.push_front({ny,nx });
-				
-			}
-			else
-			{
-				map[snake.back().first][snake.back().second] = 0;
-				snake.push_front({ ny,nx });
-				snake.pop_back();
-				
-			}
-			map[ny][nx] = 2;
-			/*
-			for (int k = 0; k < N; k++)
-			{
-				for (int l = 0; l < N; l++)
-				{
-					printf("%d ", map[k][l]);
-				}
-				printf("\n");
-			}*/
-
-			y = ny;
-			x = nx;
-			seconds++;
-
-		}
-		if (c == 'L')
-		{
-			//cout<<"good";
-			dir = (dir + 1) % 4;
-		}
-		else
-		{
-			dir = (dir - 1);
-			if (dir < 0) dir=dir + 4;
-		}
-		
-		if (state == 0)break;
-	}
-	if (state == 1)
-	{
-		while (1)
-		{
-
-			int ny = snake.front().first + dy[dir];
-			int nx = snake.front().second + dx[dir];
-			//cout << ny << " " << nx << " " << dir << endl;
-			if (ny<0 || nx<0 || ny>N - 1 || nx>N - 1 || map[ny][nx] == 2)
-			{
-				state = 0;
-				break;
-			}
-
-			if (map[ny][nx] == 1)
-			{
-				snake.push_front({ ny,nx });
-
-			}
-			else
-			{
-				map[snake.back().first][snake.back().second] = 0;
-				snake.push_front({ ny,nx });
-				snake.pop_back();
-
-			}
-			map[ny][nx] = 2;
-			/*
-			for (int k = 0; k < N; k++)
-			{
-				for (int l = 0; l < N; l++)
-				{
-					printf("%d ", map[k][l]);
-				}
-				printf("\n");
-			}
-			*/
-			y = ny;
-			x = nx;
-			seconds++;
-
-		}
-		
-	}
-
-
-	cout << seconds+1;
+bool isEnd(int cur_y, int cur_x) {
+	return cur_y < 0 || cur_y == n || cur_x < 0 || cur_x == n || visited[cur_y][cur_x];
 }
 
+void move(int start_y, int start_x) {
+
+	
+
+	// 첫 지점 이동 방향
+	int cur_dir_y = 0;
+	int cur_dir_x = 1;
+
+	queue<pair<int, int>>q;
+	q.push({start_y, start_x});
+	tails.push({ start_y,start_x });
+	visited[start_y][start_x] = true;
+
+	while (!q.empty()) {
+
+		int cur_y = q.front().first;
+		int cur_x = q.front().second;
+		
+		//cout << cur_y <<" "<< cur_x << "\n";
+		q.pop();
+
+		int next_y = cur_y + cur_dir_y;
+		int next_x = cur_x + cur_dir_x;
+
+		// 다음칸이 뱀의 몸이거나 벽이면 종료
+		if (isEnd(next_y, next_x)) {
+			cout << map[cur_y][cur_x] + 1;
+			return;
+		}
+
+		q.push({ next_y,next_x });
+		tails.push({ next_y,next_x });
+
+		int cur_tail_y = tails.front().first;
+		int cur_tail_x = tails.front().second;
+
+		// 다음 칸이 사과가 없으면? -> 꼬리 부분 false로
+		if (map[next_y][next_x] != -1) {
+			visited[cur_tail_y][cur_tail_x] = false;
+			if (!tails.empty()) {
+
+				tails.pop();
+			}
+		}
+
+		// 다음 칸의 값(시작 시간으로 부터 진행시간)이 뱀의 방향 전환할 시간이라면
+		map[next_y][next_x] = map[cur_y][cur_x] + 1;
+		visited[next_y][next_x] = true;
+
+		// 현재 방향 전환을 해야하는가 확인 후 실행
+		if (!dir_changes.empty()) {
+
+			int cur_dir_change_time = dir_changes.front().first;
+			int cur_dir_change_rotation = dir_changes.front().second;
+			if (map[next_y][next_x] == cur_dir_change_time) {
+				dir_changes.pop();
+				// 왼쪽이면?
+				// (0,1) -> (-1,0) -> (0,-1) -> (1,0)
+				if (cur_dir_change_rotation == 'L') {
+					if (cur_dir_y == 0) {
+						cur_dir_y = (-1) * (cur_dir_x);
+						cur_dir_x = 0;
+					}
+
+					else if (cur_dir_x == 0) {
+						cur_dir_x = cur_dir_y;
+						cur_dir_y = 0;
+					}
+
+				}
 
 
+				// 오른쪽이면?
+				// (0,1) -> (1,0) -> (0,-1) -> (-1,0)
+				else if (cur_dir_change_rotation == 'D') {
+					if (cur_dir_y == 0) {
+						cur_dir_y = cur_dir_x;
+						cur_dir_x = 0;
+					}
+
+					else if (cur_dir_x == 0) {
+						cur_dir_x = (-1) * cur_dir_y;
+						cur_dir_y = 0;
+					}
+				}
+			}
+		}
 
 
+	}
+}
 
+int main() {
+	cin >> n;
+	cin >> k;
+
+	for (int i = 0; i < k; i++) {
+		cin >> y >> x;
+		// 사과가 있는 곳은 -1
+		map[y-1][x-1] = -1;
+	}
+
+	cin >> l;
+
+	for (int i = 0; i < l; i++) {
+		cin >> t >> c;
+		dir_changes.push({ t,c });
+	}
+
+	move(0, 0);
+	
+
+	return 0;
+}
