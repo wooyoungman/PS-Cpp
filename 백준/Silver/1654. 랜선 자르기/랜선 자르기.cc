78c41c5e29cff0bf11940344700101172e2af9a1
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-02-13 13:32:00 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,45 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include<algorithm>
-#include<vector>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#define INF 987654321
 using namespace std;
+
+long long line[10000];
+
+long long K, N;
+long long  answer;
+
+
+void func(long long start, long long end)
+{
+	if (start > end )
+	{
+		return;
+	}
+
+	long long mid = (start + end) / 2;
+	if (mid == 0)
+	{
+		mid=mid + 1;
+	}
+	//cout << mid << endl;
+	long long temp = 0; //선개수
+	long long temp2 = 0; //mid+1 선개수
+	long long temp3 = 0;
+	for (int i = 0; i < K; i++)
+	{
+		temp=temp+line[i] / mid;
+	}
+
+	for (int i = 0; i < K; i++)
+	{
+		temp2 = temp2 + line[i] / (mid+1);
+	}
+
+	/*
+	for (int i = 0; i < K; i++)
+	{
+		temp3 = temp3 + line[i] / (mid -1);
+	}*/
+
+	//cout << temp <<" " << temp2 <<" "<<temp3 << endl;
+
+	if (temp >= N && temp2<N)
+	{
+		answer = mid;
+		return;
+	}
+	else if (temp>=N )
+	{
+		func(mid + 1, end);
+		//cout << endl << "1" << endl;
+		
+	}
+	else 
+	{
+		
+		func(start, mid - 1);
+		
+	}
+
+}
+
+
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int k, n,length,ans=0;
-	vector<int>v;
-	cin >> k >> n;
-	for (int i = 0; i < k; i++)
+	cin >> K >> N;
+	for (int i = 0; i < K; i++)
 	{
-		cin >> length;
-		v.push_back(length);
+		scanf("%d", &line[i]);
 	}
-	sort(v.begin(), v.end());
-	unsigned int max_length = v[k-1];
-	int left = 1;
-	unsigned int right = max_length, sum_piece = 0;
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		sum_piece = 0;
-		for (int i = 0; i < k; i++)
-		{
-			sum_piece += v[i] / mid;
-		}
-		if (sum_piece >= n)
-		{
-			left = mid + 1;
-			if (ans < mid)
-			{
-				ans = mid;
-			}
-		}
-		else
-		{
-			right = mid - 1;
-		}
-	}
-	cout << ans;
-	return 0;
+	func(0, 21e9);
+	cout << answer;
 }
+
+
+
