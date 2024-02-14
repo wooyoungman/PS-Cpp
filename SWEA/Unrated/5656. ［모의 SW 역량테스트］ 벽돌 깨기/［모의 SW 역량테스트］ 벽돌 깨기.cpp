133369f936c5d5a6b4c029258d43f2e2885a8704
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-02-14 13:31:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 148 ms, Memory: 13,564 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.cpp
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.cpp
@@ -1,178 +1,183 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<cstring>
-#include<queue>
-#include<limits.h>
-#define MAX INT_MAX
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#define INF 987654321
 using namespace std;
 
-int T, N, W, H;
+int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
-int dx[4] = { 0,0,-1,1 };
+int N, W,H;
+int answer;
+int map[15][12];
 
-int stone[15][20];
-// 하나의 구슬로 폭파시키는 경우 임시 맵을 통해 재귀 함수 적용
-int temp_stone[15][20];
+int minn = 21e8;
 
-int save_top[5];
-int cnt_top;
-int ans;
-struct Stone_info {
-    int y, x, cnt;
+struct poctane {
+	int y;
+	int x;
+	int pow;
+
 };
 
-void init() {
-    
-    memset(temp_stone, 0, sizeof(temp_stone));
-    memset(stone, 0, sizeof(stone));
-    memset(save_top, 0, sizeof(save_top));
-    cnt_top = 0;
-    ans = MAX;
-}
 
-void input() {
+queue<poctane> q;
 
-    cin >> N >> W >> H;
-    for (int y = 0; y < H; y++) {
-        for (int x = 0; x < W; x++) {
-            cin >> stone[y][x];
-        }
-    }
+stack<int> qq;
+void boom(int y,int x)
+{
+	q.push({ y,x,map[y][x]});
+	map[y][x] = 0;
 
-}
+	while (!q.empty())
+	{
+		y = q.front().y;
+		x = q.front().x;
+		int power = q.front().pow;
+		q.pop();
 
-void down_stone(){
+		
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 1; j <= power - 1; j++)
+			{
+				int ny = y + dy[i]*j;
+				int nx = x + dx[i]*j;
 
-    // 벽돌을 내리는 경우는 빈칸 위에 1이상인 벽돌이 있는 경우이다.
-    // 가장 아래칸부터 (현재 자신 - 1)부터 0까지 벽돌 값이 1이상인 경우를 찾는다.
-    // 찾았으면 값을 바꾼다.
-    // 0번까지 이 과정을 반복한다.
+				if (ny<0 || nx<0 || ny>H - 1 || nx>W - 1) break;
 
-    for (int i = 0; i < W; i++) {
-        for (int j = H - 1; j >= 0; j--) {
-            // temp_stone[j][i]==0인 경우를 찾았으면 (현재 빈칸인 곳을 찾았으면) 0번까지 벽돌이 있는지 확인(1이상인지)
-            if (temp_stone[j][i] == 0) {
-                for (int k = j - 1; k >= 0; k--) {
-                    // 빈칸에서 가장 가까운 벽돌과 값 변경 후 j+1후 반복문 다시 재생 
-                    if (temp_stone[k][i] > 0) {
-                        temp_stone[j][i] = temp_stone[k][i];
-                        temp_stone[k][i] = 0;
-                        break;
-                    }
-                }
-            }
-        }
-    }
+				if (map[ny][nx] == 0) continue;
 
-}
+				if (map[ny][nx] == 1)
+				{
+					map[ny][nx] = 0;
+					continue;
+				}
+				
+				q.push({ ny,nx,map[ny][nx]});
+				map[ny][nx] = 0;
 
-void func() {
+			}
+		}
+	}
 
+	for (int i = 0; i < W; i++)
+	{
+		for (int j = 0; j < H; j++)
+		{
+			if (map[j][i] != 0) qq.push(map[j][i]);
+			map[j][i] = 0;
+		}
+		int temp = H-1;
+		while (!qq.empty())
+		{
 
-    // save_top에 현재 구슬을 떨어뜨릴 위치가 선정이 완료된 상태
+			map[temp][i] = qq.top();
+			qq.pop();
+			temp--;
+		}
 
-    // 하나의 구슬 조합에 대해 탐색할 벽돌 임시 맵 생성
-    // 모든 구슬 조합에 대해 최대로 많이 부수는 경우를 탐색해야하기 때문
-    memcpy(temp_stone, stone, sizeof(temp_stone));
-    int cnt = 0;
-    // N개의 구슬 쏘기
-    for (int i = 0; i < N; i++) {
-        // 처음 터지는 벽돌을 저장하고, 이에 대해 DFS 재진행
-        queue<Stone_info>q;
-
-        for (int j = 0; j < H; j++) {
-            // save_top[i] 열에 가장 먼저 벽돌이 있는 행에서 처음으로 터짐
-            if (temp_stone[j][save_top[i]] > 0) {
-                // 가장 처음 터지는 벽돌의 좌표와 벽돌 값 저장
-                q.push({ j,save_top[i],temp_stone[j][save_top[i]] });
-                temp_stone[j][save_top[i]] = 0;
-                // 하나의 구슬에 대해서 가장 먼저 터지는 벽돌을 찾았으니 반복문 종료 후 폭파 시작
-                break;
-            }
-        }
-
-        while (!q.empty()) {
-            // 가장 먼저 터진 벽돌 꺼내기
-            Stone_info cur = q.front();
-            q.pop();
-
-            // 상하좌우 탐색
-            for (int j = 0; j < 4; j++) {
-                Stone_info next = cur;
-                // 하나의 방향에 대해서 현재 벽돌 값 -1만큼 연속 폭파
-                for (int k = 0; k < cur.cnt-1; k++) {
-                    next.y += + dy[j];
-                    next.x += + dx[j];
-
-                    // 허용 범위 확인 (더이상 못가면 현재 방향에 대해서 폭파 중지)
-                    if (next.y < 0 || next.x < 0 || next.y >= H || next.x >= W) break;
-
-                    // 벽돌이 아닌 경우 (벽돌 값 0인 경우)
-                    if (temp_stone[next.y][next.x] == 0) {
-                        // 폭파 중지가 아닌 건너 뛰기
-                        continue;
-                    }
-
-                    // 터지는 벽돌인 경우 큐에 담아서 다음 폭파 진행
-                    // 벽돌의 좌표, 값 그대로 담고 벽돌 값 0으로 변경(큐에는 정상으로 들어가지만, 폭파를 완료했다는 의미로 0)
-                    next.cnt = temp_stone[next.y][next.x];
-                    q.push(next);
-                    temp_stone[next.y][next.x] = 0;
-
-                }
-            }
-        }
-
-        // 하나의 구슬로 폭파 완료 후 위에 벽돌 밑으로 떨어뜨리기
-        down_stone();
-
-    }
-    // 결과 확인
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            if (temp_stone[i][j] > 0) {
-                cnt++;
-            }
-        }
-    }
-    if (ans > cnt) {
-        ans = cnt;
-    }
-
-}
+	}
 
 
-void choose_top(int depth) {
-    if (depth == N) {
-        // 현재 N개의 출발 지점 선택이 완료
-        // dfs 시작
-        func();
-        return;
-    }
-    for (int i = 0; i < W; i++) {
-        save_top[depth] = i;
-        choose_top(depth + 1);
-    }
 
 
 }
 
-int main() {
-   // freopen("sample_input.txt", "r", stdin);
-    cin >> T;
-    for (int i = 1; i <= T; i++) {
-        init();
-        input();
+void check()
+{
 
-        // 0~W-1까지의 열 중 중복순열로 떨어뜨릴 위치를 구한다.
-        choose_top(0);
-        
+	int cnt = 0;
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < W; j++)
+		{
+			if (map[i][j] != 0)
+			{
+				cnt++;
+			}
+		}
+	}
+	/*
+	printf("\n");
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < W; j++)
+		{
+			printf("%d ", map[i][j]);
+		}
+		printf("\n");
+	}*/
 
-        cout << "#" << i << " " << ans << "\n";
-        
-    }
+	minn = min(cnt, minn);
 
-    return 0;
+
 }
+
+
+void func(int dep)
+{
+	int temp[15][12] = { 0, };
+	if (dep == N)
+	{
+		check();
+		return;
+	}
+
+	copy(&map[0][0], &map[0][0] + 180, &temp[0][0]);
+
+	for (int i = 0; i < W; i++)
+	{
+		int state = 0;
+		for (int j = 0; j < H; j++)
+		{
+			
+			if (map[j][i] != 0)
+			{
+				boom(j,i);
+				state = 1;
+				break;
+			}
+		}
+		if (state == 0) continue;
+		func(dep + 1);
+		copy(&temp[0][0], &temp[0][0] + 180, &map[0][0]);
+
+	}
+
+}
+
+
+int main()
+{
+	//freopen("sample_input.txt", "r", stdin);
+	int T;
+	cin >> T;
+	for (int k = 1; k <= T; k++)
+	{
+		minn = 21e8;
+		
+		cin >> N >> W >> H;
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				scanf("%d", &map[i][j]);
+			}
+		}
+
+
+		func(0);
+		if (minn == 21e8)
+		{
+			minn = 0;
+		}
+		printf("#%d %d\n", k, minn);
+	}
+}
+
+
+
