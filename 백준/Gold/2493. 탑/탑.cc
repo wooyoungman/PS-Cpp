98e35e49eab79f00c0d8c7d70103e9860312f47f
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-05-07 15:29:53 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,34 +1,29 @@
 #include<iostream>
-#include<vector>
 #include<stack>
 using namespace std;
+
 int n;
-vector<int>nums(500001);
+stack<pair<int,int>>s;
 void func() {
-	stack < pair<int, int>>s;
+
 	for (int i = 0; i < n; i++) {
-		cin >> nums[i];
-		if (s.empty()) {
-			s.push({ nums[i],i+1 });
-			cout << 0 << " ";
-		}
-		else {
-			while (!s.empty()) {
-				// 수신탑 찾기
-				if (nums[i] < s.top().first) {
-					cout << s.top().second << " ";
-					break;
-				}
+		int num;
+		cin >> num;
+		while (!s.empty()) {
+			if (s.top().first < num) {
 				s.pop();
 			}
-			// 수신탑 없으면
-			if (s.empty()) {
-				cout << 0 << " ";
+			else {
+				cout << s.top().second << " ";
+				s.push({ num,i + 1 });
+				break;
 			}
-			s.push({ nums[i],i + 1 });
+		}
+		if (s.empty()) {
+			cout << 0 << " ";
+			s.push({num,i+1});
 		}
 	}
-	
 }
 int main() {
 	cin.tie(0);
