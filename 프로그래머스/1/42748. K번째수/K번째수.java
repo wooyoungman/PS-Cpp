67f76ac1da70f9a3bcbc77197e58a636a3158220
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-09-20 15:51:37 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,18 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for(int i=0;i<commands.length;i++){
-            int cnt_temp=0;
-            int start_idx=commands[i][0];
-            int end_idx=commands[i][1];
-            int pick_idx=commands[i][2];
-            int[] temp= new int[end_idx-start_idx+1];
-            for(int j=start_idx-1;j<end_idx;j++){
-                temp[cnt_temp++]=array[j];
+        
+        int commandsSize=commands.length;
+        int[] answer=new int[commandsSize];
+        int idx=0;
+        for(int i=0;i<commandsSize;i++){
+            int a=commands[i][0];
+            int b=commands[i][1];
+            int c=commands[i][2];
+            ArrayList<Integer>nums = new ArrayList<>();
+            for(int j=a-1;j<b;j++){
+                nums.add(array[j]);
             }
-            Arrays.sort(temp);
-            answer[i]=temp[pick_idx-1];
+            Collections.sort(nums);
+            answer[idx++]=nums.get(c-1);
         }
         return answer;
     }
