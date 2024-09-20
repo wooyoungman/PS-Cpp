acc09a8c1aa45e6a7c87a14c85b5e4bbd2bdd252
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-09-20 19:15:44 +0900

    [level 3] Title: 네트워크, Time: 1.16 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,39 @@
 import java.util.*;
 class Solution {
+    ArrayList<Integer>[] graph;
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        // DFS로 각 지점별로 끝까지 연결된 부분을 탐색한다.
-        // 탐색하면서 방문 여부를 체크하고, 방문하지 않은 곳을 들어갈 때 마다 +1
-        boolean [] check = new boolean[n];
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
         for(int i=0;i<n;i++){
-            check[i]=false;
+            graph[i] = new ArrayList<>();
         }
-        for(int i=0;i<n;i++){
-            if(!check[i]){
-                answer++;
-                dfs(i,computers,check);
+        
+        for(int i=0;i<computers.length;i++){
+            for(int j=0;j<computers.length;j++){
+                if(computers[i][j]==1){
+                    graph[i].add(j);
+                    graph[j].add(i);
+                }
             }
         }
+        
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i);
+                answer++;
+            }
+        }
+        
         return answer;
     }
-    public void dfs(int depth,int [][]computers, boolean []check){
-        check[depth]=true;
-        for(int i=0;i<computers.length;i++){
-            if(!check[i]&&computers[depth][i]==1){
-                dfs(i,computers,check);
-            }
+    
+    public void dfs(int v){
+        visited[v]=true;
+        for(int next : graph[v]){
+            if(!visited[next])dfs(next);
         }
     }
+    
 }
