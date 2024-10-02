0a8332982bcbe1cb8d7a291c5caa3354ac82b4aa
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-10-02 19:16:47 +0900

    [level 2] Title: 소수 찾기, Time: 810.52 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,65 +1,57 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <set>
 #include <iostream>
+#include <set>
 using namespace std;
 
-// 소수? -> 1과 자기 자신을 제외한 약수가 존재하지 않는 자연수 
-char temp_num[8];
-set<int> all_numbers;
-vector<bool> check;
-int cur_length;
-bool func(int num){
-    // 0,1일 때는 확인 x 
-    if(num<2){
-        return false;
-    }
-    int n=2;
-    while(n<num){
-        if(num%n==0){
-            return false;
-        }
-        n++;
-    }
-    // 자기자신 -1 까지 나누어 떨어지는 수가 없으면 소수 판단 -> true 반환 
-    return true;
-}
+// 1. numbers의 길이가 n일 때, 길이가 1부터 n까지 모든 순열의 경우의 수 구하기
+// 2. 첫번 째 자리가 0이 아닌 경우, set에 저장 (stoi)로 정수로 저장 (최대 7자리 -> int 가능)
+// 3. set에 저장된 모든 값에 대해 약수의 개수 구하기 (1부터 자기자신까지 나누었을 때 나머지가 0인 경우가 2인 것만)
 
-void permu(string numbers,int depth){
-    if(depth==cur_length){
-        if(temp_num[0]!='0'){
-            all_numbers.insert(stoi(temp_num));
+char temp_str[8];
+set<int> all_nums;
+bool check[8];
+void permu(string numbers, int len, int depth){
+    if(len==depth){
+        if(temp_str[0]!='0'){
+            all_nums.insert(stoi(temp_str));
         }
         return;
     }
+    
     for(int i=0;i<numbers.length();i++){
         if(!check[i]){
+            temp_str[depth]=numbers[i];
             check[i]=true;
-            temp_num[depth]=numbers[i];
-            permu(numbers, depth+1);
+            permu(numbers,len,depth+1);
             check[i]=false;
         }
     }
+    
 }
 
-// 1. 소수 판단 함수 만들기
-// 2. numbers 문자열에서 만들 수 있는 모든 수 만들기 
-// 2-1. 순열을 이용해서 만들 수 있는 수를 저장하기
-int solution(string numbers) {
-    int answer = 0;
-    for(int i=0;i<numbers.length();i++){
-        check.push_back(false);
-    }
-    for(int i=0;i<numbers.length();i++){
-        cur_length++;
-        permu(numbers, 0);
-        for(int j=0;j<check.size();j++){
-            check[j]=false;
+int countAns(){
+    int ans=0;
+    for(auto p:all_nums){
+        int cnt=0;
+        //cout<<p<<"\n";
+        for(int i=1;i<=p;i++){
+            if(p%i==0)cnt++;
+        }
+        if(cnt==2){
+            ans++;
+            //cout<<p<<" ";
         }
     }
-    for(auto p:all_numbers){
-        if(func(p))answer++;
+    return ans;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    
+    for(int i=1;i<=numbers.length();i++){
+        permu(numbers,i,0);
     }
-    return answer;
+    
+    return countAns();
 }
