f9d1b22f217b1b89c2e35e593da28de270fe3459
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-10-30 21:56:50 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 140 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,46 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-
 int n;
-vector<int> ans;
-vector<int> arr;
+vector<int>v1;
+vector<int>v2;
+
+// 참조자로 넣어야만 함수 호출 시 값 복사를 위한 시간이 발생하지 않는다
+int bs(vector<int>&v, int left, int right, int num) {
+	while (left < right) {
+		int mid = (left + right) / 2;
+		if (num > v[mid])left = mid + 1;
+		else right = mid;
+	}
+	return right;
+}
+
+void func() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		v1.push_back(num);
+	}
+	v2.push_back(v1[0]);
+	
+	int idx = 0;
+
+	for (int i = 1; i < n; i++) {
+		if (v1[i] > v2[idx]) {
+			v2.push_back(v1[i]);
+			idx = v2.size() - 1;
+		}
+		else {
+			int pos = bs(v2, 0, idx, v1[i]);
+			v2[pos] = v1[i];
+		}
+	}
+	cout << v2.size();
+}
 
 int main() {
-
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		arr.push_back(a);
-	}
-	ans.push_back(arr[0]);
-	for (int i = 1; i < n; i++) {
-		if (arr[i] > ans.back()) {
-			ans.push_back(arr[i]);
-		}
-		else {
-			int l = 0, r = ans.size() - 1, mid = 0;
-			while (l < r) {
-				mid = (l + r) / 2;
-				if (ans[mid] >= arr[i])
-				{
-					r = mid;
-				}
-				else
-				{
-					l = mid + 1;
-				}
-			}
-			ans[r] = arr[i];
-		}
-	}
-	cout << ans.size();
+	func();
 	return 0;
 }
